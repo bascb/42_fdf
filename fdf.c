a13e8e19e6ea6fb552ddb51434acff72b625dbde
Bernardo <bercbranco@gmail.com>
2023-07-08 15:10:22 +0000

    Closing window

--- fdf.c
+++ fdf.c
@@ -6,21 +6,34 @@
 /*   By: bcastelo <bcastelo@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/01 19:22:06 by bcastelo          #+#    #+#             */
-/*   Updated: 2023/07/02 18:43:46 by bcastelo         ###   ########.fr       */
+/*   Updated: 2023/07/08 15:06:58 by bcastelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <mlx.h>
 
+typedef struct	s_vars {
+	void	*mlx;
+	void	*win;
+}				t_vars;
+
+int	close_window(int keycode, t_vars *vars)
+{
+	ft_printf("Key pressed: %i - %#x\n", (unsigned int) keycode, (unsigned int) keycode);
+	if (keycode == XK_Escape)
+		ft_printf("Macro: %#x keymap: %#x\n", XK_Escape, keycode);
+	mlx_destroy_window(vars->mlx, vars->win);
+	exit(0);
+}
+
 int	main(void)
 {
-	void	*mlx;
-	void	*mlx_win;
+	t_vars	vars;
 
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
-	(void)mlx_win;
-	mlx_loop(mlx);
-	return (0);
+	vars.mlx = mlx_init();
+	vars.win = mlx_new_window(vars.mlx, 1920, 1080, "Hello world!");
+	mlx_hook(vars.win, 2, 1L<<0, close_window, &vars);
+	mlx_loop(vars.mlx);
+	exit(0);
 }
